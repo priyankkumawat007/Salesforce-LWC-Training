10975346b64b390425cf5312e87923f57ca0c97d
priyankkumawat007 <priyank.kumawat@fexle.com>
2025-01-27 18:35:18 +0530

    Remove consoles

--- Assignment1/src/lwc/parentComponent/parentComponent.js
+++ Assignment1/src/lwc/parentComponent/parentComponent.js
@@ -3,7 +3,6 @@ import { LightningElement } from 'lwc';
 export default class ParentComponent extends LightningElement {
     message="Hello World!";
     handleChange(event){
-        console.log('Hii');
         this.message = event.target.value;
     }
 }
